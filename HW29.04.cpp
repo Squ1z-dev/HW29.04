bd5c62ba59db2ec767b0a83cf5c7da434c821e6f
Squ1z-dev <maxshevthuk1604@gmail.com>
2025-05-27 10:39:33 +0300

    update

--- HW29.04.cpp
+++ HW29.04.cpp
@@ -4,11 +4,7 @@
 using namespace std;
 int main()
 {
-	//ifstream f(); // для читання
-//ofstream g(); // для запису
-// ios::app режим додавання інфи до файлу
-// ios::in режим для читання файлу
-	//Taks1 copy files
+	
 	ofstream number("D:\\cplusplus\\number2.txt", ios::app );
 	if (!number) {
 		cerr << "Error opening file!" << endl;
@@ -22,7 +18,7 @@ int main()
 	ifstream read("D:\\cplusplus\\number2.txt");
 	ofstream write("D:\\cplusplus\\number3.txt", ios::app);
 	if (!read || !write) {
-		cerr << "Error opening file!" << endl;//що таке cerr я не знаю, це мені підказка висколчила
+		cerr << "Error opening file!" << endl;//I dont khow what is cerr;
 		return 1;
 	}
 	string line;
